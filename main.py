bd4c41aa41f66c923e8dbc7baaee72eabfd0e39d
zulufoxtrot <youri.lcd@gmail.com>
2023-09-23 20:24:16 +0200

    optimize discounted loan search

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ if __name__ == "__main__":
 
     browser.login()
 
+    loans = []
+
     # first look for secondary loans with a discount
     # or no discount but still more interesting than new loans because term should be closer
-    secondary_loans_with_discount = browser.get_available_loans(MarketType.SECONDARY, maximum_premium=-0.5)
-    secondary_loans_without_discount = browser.get_available_loans(MarketType.SECONDARY, maximum_premium=0)
+    for max_premium in [-2, -1.5, -1, -0.5, 0]:
+        loans += browser.get_available_loans(MarketType.SECONDARY, maximum_premium=max_premium)
 
     # then look for primary loans
-    primary_loans = browser.get_available_loans(MarketType.PRIMARY)
-
-    loans = secondary_loans_with_discount + primary_loans + secondary_loans_without_discount
+    loans += browser.get_available_loans(MarketType.PRIMARY)
 
     for loan in loans:
         try:
